1e6d0605354940efffc6289f4df4e6b5cd7f7616
유성윤 <94370559+3x-haust@users.noreply.github.com>
2024-09-28 03:36:08 +0900

    [level 0] Title: 대문자와 소문자, Time: 5.20 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,7 @@
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string) {
+        return my_string.chars().mapToObj(c -> (char) c).map(c -> Character.isLowerCase(c) ? Character.toUpperCase(c) : Character.toLowerCase(c)).map(String::valueOf).collect(Collectors.joining());    
+    }
+}
