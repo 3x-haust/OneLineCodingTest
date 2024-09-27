ba966f538a2c473be2dd25d43ebfc0e52994c895
유성윤 <94370559+3x-haust@users.noreply.github.com>
2024-09-28 03:20:27 +0900

    [level 0] Title: 꼬리 문자열, Time: 2.40 ms, Memory: 68.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,8 @@
+import java.util.stream.Collectors;
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        return Arrays.stream(str_list).filter(s -> !s.contains(ex)).collect(Collectors.joining());
+    }
+}
