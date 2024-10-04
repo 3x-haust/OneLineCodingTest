c4c67cdabc0f9857ad1e3c0bf41b102328d8b465
유성윤 <94370559+3x-haust@users.noreply.github.com>
2024-10-04 09:24:24 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 7.15 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        return Arrays.stream(numbers).sorted().skip(numbers.length - 2).reduce((a, b) -> a * b).getAsInt();
+    } 
+}
